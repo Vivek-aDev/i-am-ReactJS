647c3545c9bf4fa54f8d0e8f658c017fbd21081a
vivekananda <vivekanandasahu.dev@gmail.com>
2025-04-15 01:37:59 +0530

    a counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // let counter = 69;
+  let [counter, setCounter] = useState(3);
+
+  function addValue() {
+    setCounter(counter + 1);
+    // counter = counter + 2;
+    // console.log("clicked", counter);
+  }
+
+  function makeZero() {
+    counter = 0;
+    setCounter(counter);
+  }
+
+  function decreaseValue() {
+    // console.log("value decreased");
+    if (counter <= 0) {
+      setCounter(counter = 0);
+    } else{
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>🧑🏻‍💻 am React</h1>
+      <h3>counter value: {counter}</h3>
+
+      <button onClick={addValue}>Add+</button>
+      <br />
+      <button onClick={makeZero}>make Zero</button>
+      <button onClick={decreaseValue}>- Subtract</button>
+    </>
+  );
+}
+
+export default App;
