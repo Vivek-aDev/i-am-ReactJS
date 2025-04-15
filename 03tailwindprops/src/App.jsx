84c7f3d2e6e8903c326ad828104a2af161f0f596
vivekananda <vivekanandasahu.dev@gmail.com>
2025-04-15 23:44:55 +0530

    configure and add tailwind css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-amber-300 font-bold underline mb-4">
+        I am react
+      </h1>
+      <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://images.pexels.com/photos/1615776/pexels-photo-1615776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+            Title
+          </span>
+          <h2 className="text-xl font-semibold tracking-wide">
+            Lorem ipsum dolor
+          </h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+          tempora ipsum soluta amet
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
