275f5ef66ee4540511e949bf831e341979db4144
vivekananda <vivekanandasahu.dev@gmail.com>
2025-04-18 23:57:10 +0530

    implement basic react router

---
+++ 07reactRouter/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as About } from "./Sections/About/About.jsx";
+export { default as Contact } from "./Sections/Contact/Contact.jsx";
+export { default as Footer } from "./Layout/Footer/Footer.jsx";
+export { default as Header } from "./Layout/Header/Header.jsx";
+export { default as Home } from "./Sections/Home/Home.jsx";
